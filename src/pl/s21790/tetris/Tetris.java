1cf24928bd3b90f8fcb9641b1f380fc9923888e9
Kamil Kaczowka <s21790@pjwstk.edu.pl>
2020-06-10 23:27:36 +0200

    make window for aplication

--- src/pl/s21790/tetris/Tetris.java
+++ src/pl/s21790/tetris/Tetris.java
@@ -1,6 +1,28 @@
 package pl.s21790.tetris;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Tetris extends JFrame {
+
+    private JLabel statusBar;
+
+
+    public Tetris() {
+        statusBar = new JLabel("0");
+        add(statusBar, BorderLayout.SOUTH);
+
+        setSize(400, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public JLabel getStatusBar() {
+        return statusBar;
+    }
+
+    public static void main(String[] args) {
+        Tetris tetris = new Tetris();
+        tetris.setLocationRelativeTo(null);
+        tetris.setVisible(true);
+    }
 }
