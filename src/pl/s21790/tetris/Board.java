fbabcfb60350312187fb6b3b726f719c20f48ca8
Kamil Kaczowka <s21790@pjwstk.edu.pl>
2020-06-11 13:13:48 +0200

    add methods to move the blocks and key support

--- src/pl/s21790/tetris/Board.java
+++ src/pl/s21790/tetris/Board.java
@@ -1,10 +1,13 @@
 package pl.s21790.tetris;
+
 import pl.s21790.tetris.Shape.Blocks;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Board extends JPanel implements ActionListener {
 
@@ -56,6 +59,47 @@ public class Board extends JPanel implements ActionListener {
         }
     }
 
+    private void blockFall() {
+        for(int i = 0; i < 4; i++) {
+            int x = curX + curBlock.x(i);
+            int y = curY - curBlock.y(i);
+            board[y * WIDTH + x] = curBlock.getShape();
+        }
+
+        completeLineRemove();
+
+        if(!fallingEnd) {
+            newBlock();
+        }
+    }
+
+    public void newBlock() {
+        curBlock.randomShapeGenerator();
+        curX = WIDTH/2 + 1;
+        curY = HEIGHT - 1 + curBlock.minY();
+
+        if(!tryMove(curBlock, curX, curY - 1)) {
+            curBlock.setShape(Blocks.emptyBlock);
+            time.stop();
+            start = false;
+            statusBar.setText("Game Over");
+        }
+    }
+
+    private void lineDown() {
+        if(!tryMove(curBlock, curX, curY - 1))
+            blockFall();
+    }
+
+    public void actionPerformed(ActionEvent actionEvent) {
+        if(fallingEnd) {
+            fallingEnd = false;
+            newBlock();
+        } else {
+            lineDown();
+        }
+    }
+
     private void drawCube(Graphics g, int x, int y, Blocks shape) {
         Color color = COLORS[shape.ordinal()];
         g.setColor(color);
@@ -72,7 +116,6 @@ public class Board extends JPanel implements ActionListener {
     public void paint(Graphics g) {
         super.paint(g);
         Dimension size = getSize();
-
         int boardTop = (int) size.getHeight() - HEIGHT * cubeHeight();
         for(int i = 0; i < HEIGHT; i++) {
             for(int j = 0; j < WIDTH; ++j) {
@@ -91,9 +134,126 @@ public class Board extends JPanel implements ActionListener {
         }
     }
 
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
+    public void start() {
+        if(isStop) {
+            return;
+        }
+        start = true;
+        fallingEnd = false;
+        linesRemoved = 0;
+        clearBoard();
+        newBlock();
+        time.start();
+    }
 
+    public void pause() {
+        if(!start) {
+            return;
+        }
+
+        isStop =! isStop;
+        if(isStop) {
+            time.stop();
+            statusBar.setText("Game Paused");
+        }else {
+            time.start();
+            statusBar.setText(String.valueOf(linesRemoved));
+        }
+        repaint();
+    }
+
+    private boolean tryMove(Shape newBlock, int newX, int newY) {
+        for(int i = 0; i < 4; ++i) {
+            int x = newX + newBlock.x(i);
+            int y = newY - newBlock.y(i);
+
+            if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) {
+                return false;
+            }
+            if (shapeAt(x, y) != Blocks.emptyBlock) {
+                return false;
+            }
+        }
+        curBlock = newBlock;
+        curX = newX;
+        curY = newY;
+        repaint();
+        return true;
+    }
+
+    private void completeLineRemove() {
+        int filledLines = 0;
+        for(int i = HEIGHT - 1; i >= 0; --i) {
+            boolean lineIsFull = true;
+            for(int j = 0; j < WIDTH; ++j) {
+                if(shapeAt(j, i) == Blocks.emptyBlock) {
+                    lineIsFull = false;
+                    break;
+                }
+            }
+            if(lineIsFull) {
+                ++filledLines;
+
+                for(int k = i; k < HEIGHT - 1; ++k) {
+                    for(int j = 0; j < WIDTH; ++j) {
+                        board[k * WIDTH + j] = shapeAt(j, k+1);
+                    }
+                }
+            }
+            if(filledLines > 0) {
+                linesRemoved += filledLines;
+                statusBar.setText(String.valueOf(linesRemoved));
+                fallingEnd = true;
+                curBlock.setShape(Blocks.emptyBlock);
+                repaint();
+            }
+        }
+    }
+
+    private void dropDown() {
+        int newY = curY;
+        while(newY > 0) {
+            if(!tryMove(curBlock, curX, newY - 1)) {
+                break;
+            }
+            --newY;
+        }
+        blockFall();
+    }
+
+    class tetrisAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if(!start || curBlock.getShape() == Blocks.emptyBlock) {
+                return;
+            }
+
+            int keyCode = e.getKeyCode();
+            if(keyCode == 'p' || keyCode == 'P'){
+                pause();
+            }
+            if(isStop){
+                return;
+            }
+
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    tryMove(curBlock, curX - 1, curY);
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    tryMove(curBlock, curX + 1, curY);
+                    break;
+                case KeyEvent.VK_DOWN:
+                    lineDown();
+                    break;
+                case KeyEvent.VK_UP:
+                    tryMove(curBlock.rotateLeft(), curX, curY);
+                    break;
+                case KeyEvent.VK_SPACE:
+                    dropDown();
+                    break;
+            }
+        }
     }
 }
 
